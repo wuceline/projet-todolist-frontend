dad845c47bf6812c69d51d8c3fb8a129c15c6704
wucéline <wuwuceline@gmail.com>
2021-08-23 14:54:06 +0200

    valider champ input

--- assets/js/components/task.js
+++ assets/js/components/task.js
@@ -2,19 +2,34 @@ const task = {
     bindSingleTaskEvents : function(taskElement) {
 
         const taskTitleElement = taskElement.querySelector(".task__title-label");
-        console.log(taskTitleElement);
-
         taskTitleElement.addEventListener("click", task.handleEnableTaskTitleEditMode);
+
+        const taskInputElement = taskElement.querySelector("input");
+
+        taskInputElement.addEventListener("keydown", task.handleValidateNewTaskTitleOnKeyDown);
+        taskInputElement.addEventListener("blur", task.handleValidateNewTaskTitleOnBlur);
+        
 
     },
 
-
-    //HANDLERS
+  // ---------------------------------------------------------
+  // Handlers
+  // ---------------------------------------------------------
     handleEnableTaskTitleEditMode: function(evt){
-
         console.log("passage en mode édition")
+        const taskTitleElement = evt.currentTarget;
+        const taskElement = taskTitleElement.closest(".task");
+        taskElement.classList.add("task--edit");
+    },
 
-    }
+    handleValidateNewTaskTitleOnKeyDown: function(evt) {
+        if(evt.key === "Enter"){
+            console.log("Champ input validé par enter");
+        };
+    },
 
+    handleValidateNewTaskTitleOnBlur: function(evt) {
+        console.log("champ input validé par perte de focus");
+    },
 
 }
