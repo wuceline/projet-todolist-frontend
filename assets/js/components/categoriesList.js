0b7f9d6c9f6ddab6d4852e734a82d82b9797c293
wucéline <wuwuceline@gmail.com>
2021-08-24 21:30:32 +0200

    liste catagories affichée avec API

--- assets/js/components/categoriesList.js
+++ assets/js/components/categoriesList.js
@@ -10,7 +10,39 @@ const categoriesList = {
     loadCategoriesFromAPI: function(){
         console.log("loadCategoriesFromAPI() initié");
 
-    },
+        let categoriesList = document.querySelector('.filters__task select');
 
+        let categoriesListForm = document.querySelector('.task__category select');
+
+        let config = {
+            method: 'GET',
+            mode: 'cors',
+            cache: 'no-cache'
+        };
+
+        fetch('https://benoclock.github.io/S07-todolist/categories.json',config)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(categories){
+
+        for (category of categories){
+            // console.log(category.name);
+
+            let listItem = document.createElement("option");
+            listItem.innerHTML= category.name;
+            categoriesList.appendChild(listItem);
+
+            let listItemForm = document.createElement("option");
+            listItemForm.innerHTML = category.name;
+            categoriesListForm.appendChild(listItemForm);
+
+
+        }
+        
+
+
+        });
+    },
 
 }
