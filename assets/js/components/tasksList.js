ab703167c2f426acfcfeab2355509ee13fdad90f
wucéline <wuwuceline@gmail.com>
2021-08-25 11:00:11 +0200

    correction tasks

--- assets/js/components/tasksList.js
+++ assets/js/components/tasksList.js
@@ -2,23 +2,29 @@ const tasksList = {
 
     init: function() {
       console.log("tasksList.init() appelé");
-      tasksList.bindAllTasksEvents();
+
+      // on désactive notre bindAllTasksEvents()
+      // car chaque tâche sera créée individuellement, à partir de l'API
+      // qui dit tâche créée individuellement, dit méthode task.createTaskElement
+      // et c'est une méthode qui "bind" déjà bien les événements d'une tâche
+      // tasksList.bindAllTasksEvents();
+
       tasksList.loadTasksFromAPI();
 
     },
 
-    bindAllTasksEvents: function(){
-        // recup les taches de la liste
-        const tasksElements = document.querySelectorAll(".tasks .task");
-        // boucle sur chaque tache de la liste
+    // bindAllTasksEvents: function(){
+    //     // recup les taches de la liste
+    //     const tasksElements = document.querySelectorAll(".tasks .task");
+    //     // boucle sur chaque tache de la liste
 
-        for(const taskElement of tasksElements) {
-        // dans la boucle, appelle task.bindSingleTaskEvents(taskElement) pour chaque tâche
-            // console.log(taskElement);
-            task.bindSingleTaskEvents(taskElement);
+    //     for(const taskElement of tasksElements) {
+    //     // dans la boucle, appelle task.bindSingleTaskEvents(taskElement) pour chaque tâche
+    //         // console.log(taskElement);
+    //         task.bindSingleTaskEvents(taskElement);
 
-        }
-    },
+    //     }
+    // },
 
     insertTaskIntoTasksList: function(newTaskElement){
       
@@ -39,19 +45,23 @@ const tasksList = {
 
       fetch('https://benoclock.github.io/S07-todolist/tasks.json', config)
       .then(function(response){
-
         return response.json();
-
       })
       .then(function(tasks){
 
-        for (let i=0; i < tasks.length; i++){
-          taskTitle = tasks[i].title;
-          taskCategory = tasks[i].category.name;
-
-          const newTask = task.createTaskElement(taskTitle, taskCategory);
-          tasksList.insertTaskIntoTasksList(newTask);
+        for(const task of tasks){
+          const taskElement = task.createTaskElement(task.title, task.category.name);
+          tasksList.insertTaskIntoTasksList(taskElement);
         }
+
+        // ce que moi j'ai fait
+        // for (let i=0; i < tasks.length; i++){
+        //   taskTitle = tasks[i].title;
+        //   taskCategory = tasks[i].category.name;
+
+        //   const newTask = task.createTaskElement(taskTitle, taskCategory);
+        //   tasksList.insertTaskIntoTasksList(newTask);
+        // }
       });
 
     },
